7065e7e8853a267b965998b293eceb9480888a43
Iñigo Zuriaga <zuriaga99@gmail.com>
2025-01-10 18:37:17 +0100

    plausible

--- src/app/pages/conversor/conversor.component.ts
+++ src/app/pages/conversor/conversor.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit,ChangeDetectorRef} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import coins from './coins.json';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ExchangeService } from '../../services/exchange.service';
 import { AuthService } from '../../services/auth.service';
 import { ChartService } from '../../services/chart.service';
@@ -27,114 +26,70 @@ export class ConversorComponent implements OnInit {
   dropdownOpenTo: boolean = false;
   filteredCurrencies: Currency[] = [];
   email: string = '';
-  lastLength: number = 0;
-
-    // Datos para el gráfico
-    lineChartData: any = { datasets: [], labels: [] };
   historyData: any[] = [];
+  lineChartData: any = { datasets: [], labels: [] };
+
   constructor(
     private exchangeService: ExchangeService,
-    private http: HttpClient,
     private authService: AuthService,
     private chartService: ChartService,
     private router: Router,
-
   ) {}
-
 
   ngOnInit() {
     this.filteredCurrencies = this.currencies;
+    this.email = this.authService.getUserEmail();
 
-    this.authService.historyData$.subscribe((data) => {
-      this.historyData = data; // Cuando el historial cambia, lo actualizamos en el componente
-    });
-
-    // this.updateChartData();
-    // this.getExchangeRate();
-    this.email= this.authService.getUserEmail();
+    // Cargar historial del usuario al iniciar
     this.authService.viewHistory(this.email).subscribe(
-      (data) => {
-        console.log("Datos recibidos:", data);
-        this.historyData = data;
-
-      },
-      (error) => {
-        console.error("Error al obtener el historial:", error);
-      });
+      (data) => this.historyData = data,
+      (error) => console.error("Error al obtener el historial:", error)
+    );
   }
+
   swapCurrencies() {
-    // Intercambiar las monedas de origen y destino
-    const temp = this.fromCurrency;
-    this.fromCurrency = this.toCurrency;
-    this.toCurrency = temp;
-
-    // Realizar la conversión nuevamente con las monedas intercambiadas
-    // this.getExchangeRate();
+    // Intercambia las monedas seleccionadas
+    [this.fromCurrency, this.toCurrency] = [this.toCurrency, this.fromCurrency];
   }
+
   async getExchangeRate() {
     try {
-      // Llamamos al servicio para obtener la tasa de cambio y esperamos la respuesta
-      const data = await this.exchangeService.getExchangeRate(this.fromCurrency.shortname, this.toCurrency.shortname, this.amount).toPromise();
-
-      console.log('Exchange rate fetched successfully', data);
-
-      // Asumimos que `data` contiene el resultado de la conversión
+      const data = await this.exchangeService.getExchangeRate(
+        this.fromCurrency.shortname, this.toCurrency.shortname, this.amount
+      ).toPromise();
+      
       this.convertedAmount = data.conversion_result;
-
-      // Recogemos el email del usuario logueado
-      const fromCoin: string = this.fromCurrency.shortname;
-      this.email = this.authService.getUserEmail();
-
-      // Actualizamos los datos del gráfico si es necesario
       this.updateChartData();
-
-      // Llamamos al servicio para crear el historial y esperar la respuesta
-      await this.createExchangeHistory(fromCoin, data.conversion_result); // Esperamos la creación del historial
-
-      // Actualizamos el historial en la vista
+      await this.createExchangeHistory(this.fromCurrency.shortname, data.conversion_result);
       this.updateHistory();
-
     } catch (error) {
       console.error('Error al obtener la tasa de cambio o crear el historial', error);
     }
   }
 
   async createExchangeHistory(fromCoin: string, conversionResult: number) {
-    // Llamamos al servicio para crear el historial
     try {
-      const response = await this.exchangeService.createExchangeHistory(
-        fromCoin,
-        this.amount,
-        this.toCurrency.shortname,
-        conversionResult,
-        new Date(),
-        this.email
+      await this.exchangeService.createExchangeHistory(
+        fromCoin, this.amount, this.toCurrency.shortname, conversionResult, new Date(), this.email
       ).toPromise();
-
-      console.log('Historial creado con éxito', response);
     } catch (error) {
       console.error('Error al crear el historial', error);
     }
   }
 
-  // async onConversionSuccess() {
-  //   try {
-  //     const email = this.authService.getUserEmail();
-  //
-  //     // Simulamos obtener el historial actualizado y lo emitimos
-  //     this.authService.fetchAndUpdateHistory(email); // Esto actualizará el BehaviorSubject
-  //   } catch (error) {
-  //     console.error('Error al obtener el historial:', error);
-  //   }
-  // }
-
-
   toggleDropdown(select: 'from' | 'to') {
-    this.dropdownOpenFrom = select === 'from' ? !this.dropdownOpenFrom : false;
-    this.dropdownOpenTo = select === 'to' ? !this.dropdownOpenTo : false;
+    // Alterna el estado del dropdown seleccionado
+    if (select === 'from') {
+      this.dropdownOpenFrom = !this.dropdownOpenFrom;
+      this.dropdownOpenTo = false;
+    } else {
+      this.dropdownOpenTo = !this.dropdownOpenTo;
+      this.dropdownOpenFrom = false;
+    }
   }
 
   selectCurrency(currency: Currency, type: 'from' | 'to') {
+    // Establece la moneda seleccionada
     if (type === 'from') {
       this.fromCurrency = currency;
     } else {
@@ -149,6 +104,7 @@ export class ConversorComponent implements OnInit {
   }
 
   filterCurrencies(event: Event) {
+    // Filtra las monedas según el texto ingresado
     const searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
     this.filteredCurrencies = this.currencies.filter(
       (currency) =>
@@ -158,96 +114,34 @@ export class ConversorComponent implements OnInit {
     );
   }
 
-  // Método para obtener la tasa de cambio
-  // getExchangeRate() {
-  //   this.exchangeService.getExchangeRate(this.fromCurrency.shortname, this.toCurrency.shortname, this.amount).subscribe(
-  //     (data) => {
-  //       console.log('Exchange rate fetched successfully', data);
-  //
-  //       this.convertedAmount = data.conversion_result; // Asume que `data` tiene la estructura adecuada
-  //
-  //       // Recogemos el email del usuario logueado
-  //       const fromCoin: string = this.fromCurrency.shortname;
-  //       this.email = this.authService.getUserEmail();
-  //
-  //       this.updateChartData();
-  //       // Llamamos al servicio para crear el historial
-  //       this.exchangeService.createExchangeHistory(
-  //         this.fromCurrency.shortname,
-  //         this.amount,
-  //         this.toCurrency.shortname,
-  //         data.conversion_result,
-  //         new Date(),
-  //         this.email
-  //
-  //           ).subscribe(
-  //             (response) => {
-  //               console.log('Historial creado', response);
-  //               this.updateHistory();
-  //             },
-  //             (error) => {
-  //               console.error('Error al crear el historial', error);
-  //             }
-  //           );
-  //     },
-  //     (error: HttpErrorResponse) => {
-  //       console.error('Error fetching exchange rate', error);
-  //     }
-  //   );
-  // }
-
   updateHistory() {
-    this.email = this.authService.getUserEmail();
     this.authService.viewHistory(this.email).subscribe(
-      (data) => {
-        console.log("Datos recibidos:", data);
-        this.historyData = data;
-
-      },
-      (error) => {
-        console.error("Error al obtener el historial:", error);
-      });
+      (data) => this.historyData = data,
+      (error) => console.error("Error al obtener el historial:", error)
+    );
   }
 
   chartReady: boolean = false;
-  updateChartData() {
-    //DESCOMENTAR PARA QUE FUNCIONE EL GRAFICO
 
+  updateChartData() {
     this.chartService.getHistoricalData(this.fromCurrency.shortname, this.toCurrency.shortname).subscribe(
       (data) => {
-        console.log('Raw data received:', data);
-
         const timeSeries = data['Time Series FX (Daily)'];
-        if (!timeSeries) {
-          console.error('Time Series data is missing in the response.');
-          return;
-        }
+        if (!timeSeries) return;
 
-        // Procesa las fechas y valores de cierre
-        const labels = Object.keys(timeSeries).reverse(); // Fechas en orden ascendente
-        const values = labels.map(date => {
-          const closeValue = timeSeries[date]?.['4. close'];
-          return closeValue ? parseFloat(closeValue) : 0; // Asegúrate de que sea un número
-        });
+        const labels = Object.keys(timeSeries).reverse();
+        const values = labels.map(date => parseFloat(timeSeries[date]?.['4. close']) || 0);
 
-        console.log('Processed labels:', labels);
-        console.log('Processed values:', values);
-
-        // Asigna los datos al gráfico
         this.lineChartData = {
-          labels, // Fechas
-          datasets: [
-            {
-              data: values, // Valores de cierre
-              label: `${this.fromCurrency.shortname} to ${this.toCurrency.shortname}`,
-              borderColor: '#3e95cd',
-              fill: false,
-            },
-          ],
+          labels,
+          datasets: [{
+            data: values,
+            label: `${this.fromCurrency.shortname} to ${this.toCurrency.shortname}`,
+            borderColor: '#3e95cd',
+            fill: false,
+          }],
         };
         this.chartReady = true;
-        // Log para verificar
-        console.log('Chart data updated:', this.lineChartData);
       },
       (error) => {
         console.error('Error fetching chart data', error);
@@ -256,10 +150,7 @@ export class ConversorComponent implements OnInit {
     );
   }
 
-
   onAmountChange() {
-    // Aquí puedes manejar los cambios en el input de cantidad si es necesario
+    // Puedes manejar cambios en el monto aquí si es necesario
   }
-
-  protected readonly history = history;
 }
